ccdebcd5397263a07dde13b0a51d62536e9b189a
0xffffharry <95022881+0xffffharry@users.noreply.github.com>
2023-10-28 13:08:51 +0800

    fix bug

--- upstream/upstream.go
+++ upstream/upstream.go
@@ -14,7 +14,7 @@ import (
 )
 
 func init() {
-	os.Setenv("QUIC_GO_DISABLE_RECEIVE_BUFFER_WARNING", " true")
+	os.Setenv("QUIC_GO_DISABLE_RECEIVE_BUFFER_WARNING", "true")
 }
 
 type Options struct {
